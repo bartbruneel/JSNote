744d5d21fda0c9adedb331c3b218fc55dbdce2d5
bartbruneel <bartbruneelbart@gmail.com>
2021-11-30 21:02:14 +0100

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkgPathPlugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState("");
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+    });
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    iframe.current.srcdoc = html;
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+  };
+
+  const html = `
+      <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+        </body>
+        <script>
+            window.addEventListener('message', (event) => {
+                try {
+                    eval(event.data);
+                } catch(err) {
+                    const root = document.querySelector('#root');
+                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                    console.error(err);
+                }
+            }, false);
+        </script>
+      </html>
+    `;
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
