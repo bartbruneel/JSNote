b4470f8236e13c6527a7da2d380474cf8c7eb9ea
bartbruneel <bartbruneelbart@gmail.com>
2021-12-01 21:58:33 +0100

    Make code editor and preview windows resizable by dragging

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import "./preview.css";
 
 interface PreviewProps {
   code: string;
@@ -6,7 +7,9 @@ interface PreviewProps {
 
 const html = `
       <html>
-        <head></head>
+        <head>
+          <style>html {background-color: white}</style>
+        </head>
         <body>
             <div id="root"></div>
         </body>
@@ -35,13 +38,15 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   };
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-      onLoad={loadHandler}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+        onLoad={loadHandler}
+      />
+    </div>
   );
 };
 
