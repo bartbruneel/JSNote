bf93a20fa90e849886fae5c24ad3c75717028065
bartbruneel <bartbruneelbart@gmail.com>
2021-12-04 17:59:56 +0100

    Add redux store and actions on cells

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+import { Provider } from "react-redux";
 import TextEditor from "./components/text-editor";
+import { store } from "./state";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
